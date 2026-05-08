b5139677b6330be6761bef82839cb81889b386c9
kavyasree28 <bhkavyasree@gmail.com>
2026-05-08 20:29:44 +0530

    Create Selectionsort.cpp

---
+++ Selectionsort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n,a[50];
+   cout<<"Enter number of elements:";
+   cin>>n;
+   cout<<"Enter elements:";
+   for(int i=0;i<n;i++)
+      cin>>a[i];
+   for(int i=0;i<n-1;i++)
+   {
+      int min=i;
+      for(int j=i+1;j<n;j++)
+      {
+         if(a[j]<a[min])
+            min=j;
+      }
+      if(min!=i)
+      {
+         int temp=a[i];
+         a[i]=a[min];
+         a[min]=temp;
+      }
+   }
+   cout<<"Sorted array:";
+   for(int i=0;i<n;i++)
+      cout<<a[i]<<" ";
+   return 0;
+}
